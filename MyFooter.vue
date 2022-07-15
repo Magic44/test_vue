275c6447a9b6300180bc05ce5dd25736c717e2bd
Magic44 <101734301+Magic44@users.noreply.github.com>
2022-07-15 22:29:26 +0800

    Add files via upload

---
+++ MyFooter.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="todo-footer" v-if="total">
+    <label>
+      <input type="checkbox" v-model="isAll" />
+    </label>
+    <span>
+      <span>已完成{{ done }}</span> / 全部{{ total }}
+    </span>
+    <button class="btn btn-danger" @click="remove">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+  props: ["removeAllDone", "changeAllTodo", "todos"],
+  methods: {
+    remove() {
+      this.removeAllDone();
+    },
+  },
+  computed: {
+    total() {
+      return this.todos.length;
+    },
+    done() {
+      // 演示用于统计数组中符合条件的元素个数
+      return this.todos.reduce((sum, todo) => (todo.done ? ++sum : sum), 0);
+    },
+    isAll: {
+      get() {
+        return this.total === this.done && this.total > 0;
+      },
+      set(val) {
+        this.changeAllTodo(val);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
